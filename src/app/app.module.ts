f23a92c0af0deda51b1c855f3ebb37789736f358
Imre Pénzes <imre-penzes@Imres-MacBook-Pro.local>
2023-04-23 20:46:09 +0200

    Implement simple alerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,11 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, MainNavigationComponent],
+  declarations: [AppComponent, MainNavigationComponent, SuccessAlertComponent, WarningAlertComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
