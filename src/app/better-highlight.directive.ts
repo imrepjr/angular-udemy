7e46d6ad8c246711fd4944452ac4520c92773238
Imre Penzes <emery90@gmail.com>
2023-05-20 20:31:08 +0200

    Make highlight directive more flexible

--- src/app/better-highlight.directive.ts
+++ src/app/better-highlight.directive.ts
@@ -3,24 +3,32 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
+  OnInit,
   Renderer2,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
-export class BetterHighlightDirective {
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor?: string;
   @HostListener('mouseenter') mouseOver(_event: Event) {
     // this.setBackgroundColor('blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
   @HostListener('mouseleave') mouseLeave(_event: Event) {
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
     // this.setBackgroundColor('transparent');
   }
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
 
   setBackgroundColor(color: string) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', color);
